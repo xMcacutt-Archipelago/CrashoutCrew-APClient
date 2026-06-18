d9fba01b3daad9f035c00d73303867e656e77523
Matthew Cacutt <matthew.cacutt@gmail.com>
2026-06-18 23:37:43 +0100

    Full contract generation

---
+++ ContractGenerator.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Aggro.Core;
+using UnityEngine;
+using Random = System.Random;
+
+namespace CrashoutCrew_APClient;
+
+public class ContractGenerator
+{
+    private static List<(string, string)> _warehouses = 
+    [
+        ("Assets/Scenes/scene-Dumbbell_1-2.unity", "Assets/Scenes/scene-Dumbbell.unity"),
+        ("Assets/Scenes/scene-IPiece_1-2.unity", "Assets/Scenes/scene-IPiece.unity"),
+        ("Assets/Scenes/scene-SBendMini_1-2.unity", "Assets/Scenes/scene-SBendMini.unity"),
+        ("Assets/Scenes/scene-ConveyorBlock.unity", "Assets/Scenes/scene-ConveyorBlock.unity"),
+        ("Assets/Scenes/scene-ConveyorHallway.unity", "Assets/Scenes/scene-ConveyorHallway.unity"),
+        ("Assets/Scenes/scene-ConveyorLoop.unity", "Assets/Scenes/scene-ConveyorLoop.unity"),
+        ("Assets/Scenes/scene-Figure8RaceTrackMini.unity", "Assets/Scenes/scene-Figure8RaceTrackMini.unity"),
+        ("Assets/Scenes/scene-LBendRaceTrackMini.unity", "Assets/Scenes/scene-LBendRaceTrackMini.unity"),
+        ("Assets/Scenes/scene-RaceTrackMini.unity", "Assets/Scenes/scene-RaceTrackMini.unity"),
+        ("Assets/Scenes/scene-UBendMiniRacetrack.unity", "Assets/Scenes/scene-UBendMiniRacetrack.unity"),
+    ];
+
+    private static int[] _outboundDeliveryCount = [4, 5, 6, 7, 8];
+
+    public static ContractObject[] GenerateContracts(
+        Random random, 
+        ShiftOrderObject[] allOrders, 
+        int twoOrderContracts,
+        int threeOrderContracts, 
+        int fourOrderContracts)
+    {
+        var contractSizes = new List<int>();
+        contractSizes.AddRange(Enumerable.Repeat(2, twoOrderContracts));
+        contractSizes.AddRange(Enumerable.Repeat(3, threeOrderContracts));
+        contractSizes.AddRange(Enumerable.Repeat(4, fourOrderContracts));
+        contractSizes = contractSizes.OrderBy(_ => random.Next()).ToList();
+        
+        var contracts = new List<ContractObject>();
+        var orders = new List<ShiftOrderObject>();
+
+        foreach (var size in contractSizes)
+        {
+            var selectedOrders = new List<ShiftOrderObject>();
+            while (selectedOrders.Count < size)
+            {
+                if (orders.Count == 0)
+                    orders.AddRange(allOrders.OrderBy(_ => random.Next()));
+                var nextOrder = orders.FirstOrDefault(o => !selectedOrders.Contains(o));
+                orders.Remove(nextOrder);
+                selectedOrders.Add(nextOrder);
+            }
+            contracts.Add(GenerateContract(random, selectedOrders.ToArray()));
+        }
+        return contracts.OrderBy(x => x.orders.Length).ToArray();
+    }
+
+    private static ContractObject GenerateContract(Random random, ShiftOrderObject[] orders)
+    {
+        var contract = ScriptableObject.CreateInstance<ContractObject>();
+        contract.title = orders.GenerateName(random);
+        contract.modifierMultiplier = 1;
+        contract.type = ContractType.Explicit;
+        contract.orders = orders;
+        var warehouses = _warehouses.Random();
+        contract.smallWarehouse = warehouses.Item1;
+        contract.bigWarehouse = warehouses.Item2;
+        contract.multiplierForOnePlayer = 0.4f;
+        contract.multiplierForTwoPlayers = 0.7f;
+        contract.multiplierForThreePlayers = 0.85f;
+        contract.shift1 = GenerateShift(random, contract, orders, 0);
+        contract.shift2 = GenerateShift(random, contract, orders, 1);
+        contract.shift3 = GenerateShift(random, contract, orders, 2);
+        contract.shift4 = GenerateShift(random, contract, orders, 3);
+        contract.shift5 = GenerateShift(random, contract, orders, 4);
+        contract.shopCards = [];
+        contract.unlocks = [];
+        contract.demoVisualPrefabs = [];
+        return contract;
+    }
+
+    private static ContractShift GenerateShift(Random random, ContractObject contract, ShiftOrderObject[] orders, int shiftIndex)
+    {
+        var shift = new ContractShift();
+        List<ContractShift.Outbound> outbounds = [];
+        List<ContractShift.Inbound> inbounds = [];
+        List<ContractShift.Order> shiftOrders = [];
+        shift.owner = contract;
+        shift.truckPatienceDuration = random.Next(90, 110);
+        
+        var outboundDeliveries = _outboundDeliveryCount[shiftIndex];
+        for (var i = 0; i < outboundDeliveries; i++)
+        {
+            var outboundBoxCount = random.Next(4, 7); 
+            outbounds.Add(new ContractShift.Outbound
+            {
+                bayCount = 1,
+                boxCount = outboundBoxCount,
+                secondsFromPrevious = random.Next(8, 14)
+            });
+            var inboundBayCount = random.Next(1, 3); 
+            var totalInboundBoxes = outboundBoxCount + random.Next(0, 3);
+            var inboundBoxCountPerBay = Mathf.CeilToInt((float)totalInboundBoxes / inboundBayCount);
+            inbounds.Add(new ContractShift.Inbound
+            {
+                bayCount = inboundBayCount,
+                boxCount = inboundBoxCountPerBay,
+                normalizedTime = i * (0.75f / outboundDeliveries),
+            });
+        }
+        shift.outbound = outbounds.ToArray();
+        shift.inbound = inbounds.ToArray();
+        shift.payOutAmount = outboundDeliveries * random.Next(75, 115);
+        foreach (var order in orders)
+        {
+            var minimum = shiftIndex < 4 ? 0 : 1;
+            shiftOrders.Add(new ContractShift.Order
+            {
+                owner = contract,
+                order = order,
+                randomOrderIndex = 1,
+                cardCount = random.Next(minimum, 3),
+            });
+        }
+        if (shiftOrders.All(x => x.cardCount == 0))
+            shiftOrders.Random(random: random)!.cardCount = 1;
+        shift.orders = shiftOrders.ToArray();
+        return shift;
+    }
+}
--- Patches/GameManager_Patches.cs
+++ Patches/GameManager_Patches.cs
@@ -18,58 +18,13 @@ public static class GameManager_Patches
             __result = __instance.demoContracts;
             return;
         }
-        var random = new Random(2);
+        var random = new Random(4);
         var allOrders = Resources.FindObjectsOfTypeAll<ShiftOrderObject>();
         // var allOrders = __instance.contracts.SelectMany(x => x.orders.ToHashSet()).ToHashSet().ToArray();
-        var twoOrderContracts = __instance.contracts
-            .Where(x => x.orders.Length == 2 && x.type == ContractType.Explicit)
-            .OrderBy(_ => random.Next()).Take(ArchipelagoHandler.Instance?.slotData?.ContractCountTwo ?? 4);
-        var threeOrderContracts = __instance.contracts.Skip(1)
-            .Where(x => x.orders.Length == 3 && x.type == ContractType.Explicit)
-            .OrderBy(_ => random.Next()).Take(ArchipelagoHandler.Instance?.slotData?.ContractCountThree ?? 12);
-        var fourOrderContracts = __instance.contracts
-            .Where(x => x.orders.Length == 4 && x.type == ContractType.Explicit)
-            .OrderBy(_ => random.Next()).Take(ArchipelagoHandler.Instance?.slotData?.ContractCountFour ?? 4);
-        var contracts = new List<ContractObject>();
-        contracts.AddRange(twoOrderContracts);
-        contracts.AddRange(threeOrderContracts);
-        contracts.AddRange(fourOrderContracts);
-        var randomizedContracts = new List<ContractObject>();
-        foreach (var contract in contracts)
-        {
-            do
-            {
-                RandomizeContract(random, allOrders, contract);
-            } while (randomizedContracts.Any(existingContract =>
-                     {
-                         var existing = existingContract.orders.Select(order => order.name).ToHashSet();
-                         var dupes = contract.orders.Count(order => !existing.Add(order.name));
-                         return dupes > 1;
-                     }));
-            randomizedContracts.Add(contract);
-        }
-        __result = randomizedContracts.ToArray();
-    }
 
-    private static void RandomizeContract(Random random, ShiftOrderObject[] allOrders, ContractObject contract)
-    {
-        var originalOrders = contract.orders.ToArray();
-        var newOrders = allOrders.OrderBy(x => random.Next()).Take(contract.orders.Length).ToArray();
-        var replacements = originalOrders
-            .Zip(newOrders, (oldOrder, newOrder) => new { oldOrder, newOrder })
-            .ToDictionary(x => x.oldOrder, x => x.newOrder);
-        contract.orders = newOrders;
-        contract.title = contract.orders.GenerateName(random);
-        contract.bellsRequired = 0;
-        foreach (var order in contract.shift1.orders)
-            order.order = replacements.TryGetValue(order.order, out var replacement) ? replacement : order.order;
-        foreach (var order in contract.shift2.orders)
-            order.order = replacements.TryGetValue(order.order, out var replacement) ? replacement : order.order;
-        foreach (var order in contract.shift3.orders)
-            order.order = replacements.TryGetValue(order.order, out var replacement) ? replacement : order.order;
-        foreach (var order in contract.shift4.orders)
-            order.order = replacements.TryGetValue(order.order, out var replacement) ? replacement : order.order;
-        foreach (var order in contract.shift5.orders)
-            order.order = replacements.TryGetValue(order.order, out var replacement) ? replacement : order.order;
+        var contracts2 = ArchipelagoHandler.Instance?.slotData?.ContractCountTwo ?? 5;
+        var contracts3 = ArchipelagoHandler.Instance?.slotData?.ContractCountThree ?? 5;
+        var contracts4 = ArchipelagoHandler.Instance?.slotData?.ContractCountFour ?? 5;
+        __result =  ContractGenerator.GenerateContracts(random, allOrders, contracts2, contracts3, contracts4);
     }
 }
